bf65ea61b4250fca41096d107bad3278ce52f9ab
MeganGuidry <mguid73@lsu.edu>
2018-11-12 18:19:41 -0800

    script updated with more methods

--- oysterM.py
+++ oysterM.py
@@ -7,13 +7,30 @@
 class oyster:
     """This class defines population tolerances for salinity and temperature and population size."""
 
-    def __init__(self, size, highSal, lowSal, highTemp, lowTemp, gen):
-        self.size = size
+    def __init__(self, startSize, startTime, highSal, lowSal, highTemp, lowTemp, generations):
+        self.startSize = startSize
+        self.startTime = startTime
         self.highSal = highSal
         self.lowSal = lowSal
         self.highTemp = highTemp
         self.lowTemp = lowTemp
-        self.generations = 
+        self.generations = [[startSize, startTime]]
 
-    def updatePop(self):
+    def currentSize(self):
+        return self.generations[self.generations.count - 1]
+
+    def updatePopulation(self):
+        newSize = self.startSize()[0]
+        if (salinity > self.highSal):		# set the 'salinity' and 'temperature' variables
+            newSize = newSize * 0.9		# ...with USGS current conditions data 
+        if (salinity < self.lowSal):
+            newSize = newSize * 0.9
+        if (temperature > self.highTemp):
+            newSize = newSize * 0.9
+        if (temperature < self.lowTemp):
+            newSize = newSize * 0.9		# will set survival percentage at more specific
+						# ...thresholds later 
+## FUTURE WORK
+# add in reproduction rate in addition to survival rate
+# pull data from USGS
 
