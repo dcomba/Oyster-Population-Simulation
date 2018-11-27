f0172b8c40ca2fae8523e8cc5aa9e5f2086d6c67
MeganGuidry <mguid73@lsu.edu>
2018-11-27 11:24:51 -0800

    updated to read in files

--- oysterM.py
+++ oysterM.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-
-#pull USGS data
+import numpy
+import numpy.random as nr
+import matplotlib.pyplot as plt
+# oyster fact sheet website: https://www.sms.si.edu/irlspec/Crassostrea_virginica.htm 
 #CONDITION THRESHOLDS FROM HOLLIS' MANUSCRIPT
 	#Salinity (H 25ppt, M 15ppt, L 7ppt)
 	#Temperature (W 35degC, C 20degC)
@@ -12,14 +14,6 @@
 #make different files with data from different sites (one data file per site)
 	#use data for salinity/temperature for all 4 sites 
 	#modeled over 2 month span from August 1, 2017-September 30, 2017 to include Hurricane Harvey
-#set up each site as a variable with all tolerances set									
-						#***SET TOLERANCES (hollis data)***
-	#VB= oyster(siteName= "Vermillion Bay", startSize= , startTime= , highSal=7.3, lowSal=1.1 , lowTemp=20, highTemp=35 )
-	#CL= oyster(siteName= "Calcaseiu Lake", startSize= , startTime= , highSal=15.5, lowSal=7.3 , lowTemp=20, highTemp=35 )
-	#GI= oyster(siteName= "Grand Isle", startSize= , startTime= , highSal=, lowSal= , lowTemp=20, highTemp=35 )
-	#LM= oyster(siteName= "LUMCON", startSize= , startTime= , highSal =, lowSal= , lowTemp=20, highTemp=35 )
-#read literature for ideas on how to format functions
-
 #store output data (population size in an array)
 
 #(several lines on one graph?)(Each condition with 4 lines for each pop?)
@@ -30,13 +24,13 @@
 class oyster:
     """This class defines population tolerances for salinity and temperature and population size."""
 
-    def __init__(self, siteName, startSize, startTime, highSal, lowSal, lowTemp=20, highTemp=35, generations):
+    def __init__(self, siteName, startSize, startTime, highSal, lowSal, lowTemp, highTemp):
         self.siteName= siteName
         self.startSize = startSize
         self.startTime = startTime
         self.highSal = highSal
         self.lowSal = lowSal
-	self.lowTemp = lowTemp
+        self.lowTemp = lowTemp
         self.highTemp = highTemp
         self.generations = [[startSize, startTime]]
 
@@ -49,19 +43,33 @@ class oyster:
             newSize = newSize * 0.9		# ...with USGS current conditions data 
         if (salinity < self.lowSal):
             newSize = newSize * 0.9
-        if (temperature > self.Temp):
+        if (temperature > self.highTemp):
             newSize = newSize * 0.9
-       						# will set survival percentage at more specific
-						# ...thresholds later 
+       	if (temperature < self.lowTemp):	# will set survival percentage at more specific
+            newSize = newSize * 0.9		# ...thresholds later 
 
-VB= oyster(siteName= "Vermillion Bay", startSize= , startTime= , highSal=7.3, lowSal=1.1 , lowTemp=20, highTemp=35 )
-CL= oyster(siteName= "Calcaseiu Lake", startSize= , startTime= , highSal=15.5, lowSal=7.3 , lowTemp=20, highTemp=35 )
-GI= oyster(siteName= "Grand Isle", startSize= , startTime= , highSal=, lowSal= , lowTemp=20, highTemp=35 )
-LM= oyster(siteName= "LUMCON", startSize= , startTime= , highSal =, lowSal= , lowTemp=20, highTemp=35 )
+VB= oyster(siteName= "Vermillion Bay", startSize=100 , startTime=0 , highSal=7.3, lowSal=1.1 , lowTemp=20, highTemp=35 )
+CL= oyster(siteName= "Calcaseiu Lake", startSize=100 , startTime=0 , highSal=15.5, lowSal=7.3 , lowTemp=20, highTemp=35 )
+GI= oyster(siteName= "Grand Isle", startSize=100 , startTime=0 , highSal=30, lowSal=16 , lowTemp=20, highTemp=35 )
+LM= oyster(siteName= "LUMCON", startSize=100 , startTime=0 , highSal=28, lowSal=14, lowTemp=20, highTemp=35 )
+# GI & LM have randomly assigned salinities atm (not in Hollis' manuscript)
 
-## FUTURE WORK
-# add in reproduction rate in addition to survival rate
-# pull data from USGS
-# one salinty, one temp and one combined affects graph
+LM_file = "LM_data.tsv"
+inFile = (LM_file, 'r')
+inFile = open(LM_file, 'r')
+LM_salinity = []
+for salinity in inFile:
+    LM_salinity.append(salinity.split(' ')[0])
+print(LM_salinity)
 
+#VB_file = "VB_data.tsv"			# make VB_data.tsv file
+#inFile = (VB_file, 'r')
+#inFile = open(VB_file, 'r')
 
+#CL_file = "CL_data.tsv"			# make CL_data.tsv file
+#inFile = (CL_file, 'r')
+#inFile = open(CL_file, 'r')
+
+#GI_file = "GI_data.tsv"			# make GI_data.tsv file
+#inFile = (GI_file, 'r')
+#inFile = open(GI_file, 'r')
