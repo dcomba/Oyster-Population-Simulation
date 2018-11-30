3d105d378ae80e99931232fbcc4781bee95a6dbd
Devin Comba <dacomba97@gmail.com>
2018-11-30 15:24:43 -0600

    A working program! with ugly graphs

--- oysterpopd.py
+++ oysterpopd.py
@@ -2,8 +2,8 @@
 # this program simulates oyster populations in locations affected by salinity and temperature
 # the goal is to visually represent the effects of freshwater events during the summer on oyster populations
 
-
 import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 #time = np.arange(2100)
 
@@ -57,39 +57,39 @@ class oysterpopulation:
         populationSizes = []
         populationSizes.append(self.startSize)
         environments = zip(salinities, temperatures)
-        salinityTimer = 10
-        temperatureTimer = 10
+        salinityTimer = 336
+        temperatureTimer = 336
         for environment in environments:
             salinity = environment[0]
             temperature = environment[1]
             newSize = populationSizes[-1]
             if (salinity < self.saltol):
-                salinityTimer = salinityTimer - 1
+                salinityTimer = salinityTimer - 48
                 if (salinityTimer <= 0):
-                    newSize = newSize * .7
+                    newSize = newSize * .90
             else:
-                salinityTimer = salinityTimer + 1
+                salinityTimer = salinityTimer + 48
             if (salinity < 1):
-                salinityTimer = salinityTimer - 1
+                salinityTimer = salinityTimer - 48
                 if (salinityTimer <= 0):
-                    newSize = newSize * .1
+                    newSize = newSize * .60
             if (temperature > self.temptol):
-                temperatureTimer = temperatureTimer - 1
+                temperatureTimer = temperatureTimer - 48
                 if (temperatureTimer <= 0):
-                    newSize = newSize * .6
+                    newSize = newSize * .95
             else:
-                temperatureTimer = temperatureTimer + 1
+                temperatureTimer = temperatureTimer + 48
             if (temperature == 20):
-                temperatureTimer = temperatureTimer - 1
+                temperatureTimer = temperatureTimer - 48
                 if (temperatureTimer <= 0):
-                    newSize = newSize * 1.1
+                    newSize = newSize * 1.5
             else:
-                temperatureTimer = temperatureTimer + 1
+                temperatureTimer = temperatureTimer + 48
             populationSizes.append(newSize)
-            if temperatureTimer > 10:
-                temperatureTimer = 10
-            if salinityTimer > 10:
-                salinityTimer = 10
+            if temperatureTimer > 336:
+                temperatureTimer = 336
+            if salinityTimer > 336:
+                salinityTimer = 336
             if temperatureTimer < 0:
                 temperatureTimer = 0
             if salinityTimer < 0:
@@ -99,31 +99,33 @@ class oysterpopulation:
 
 # next step is to make our list of lists (salinites, temperatures, populations)
 
-class timesdata:
+#class timesdata:
     
-    def pulltimes(self, fileName, columnIndex):
-        data = []
-        delimiter = "|"
-        with open(fileName) as file:
-            for line in file.readlines():
-                line = line.split(delimiter)[columnIndex]
-                data.append(line)
-        return data
+    #def pulltimes(self, fileName, columnIndex):
+        #data = []
+        #delimiter = "|"
+        #with open(fileName) as file:
+            #for line in file.readlines():
+                #line = line.split(delimiter)[columnIndex]
+                #data.append(line)
+        #return data
 
-times = timesdata()
-timesforplot = times.pulltimes("CRAugSep.txt", 0)
+time = np.arange(2177)
 
-VB = oysterpopulation("Vermillion Bay", 50, 2, 25)
+#times = timesdata()
+#timesforplot = times.pulltimes("CRAugSep.txt", 0)
+
+VB = oysterpopulation("Vermillion Bay", 10000, 2, 25)
 VBsalinities = VB.pullsalinity("VBAugSep.txt")
 VBtemperatures = VB.pulltemperature("VBAugSep.txt")
 VBpopulation = VB.simulatePopulation(VBsalinities, VBtemperatures)
 
-CR = oysterpopulation("Calcasieu River", 50, 5, 25)
+CR = oysterpopulation("Calcasieu River", 10000, 5, 25)
 CRsalinities = CR.pullsalinity("CRAugSep.txt")
 CRtemperatures = CR.pulltemperature("CRAugSep.txt")
 CRpopulation = CR.simulatePopulation(CRsalinities, CRtemperatures)
 
-CB = oysterpopulation("Caillou Bay", 50, 15, 25)
+CB = oysterpopulation("Caillou Bay", 10000, 15, 25)
 CBsalinities = CB.pullsalinity("CBAugSep.txt")
 CBtemperatures = CB.pulltemperature("CBAugSep.txt")
 CBpopulation = CB.simulatePopulation(CBsalinities, CBtemperatures)
@@ -133,8 +135,10 @@ CBpopulation = CB.simulatePopulation(CBsalinities, CBtemperatures)
 #for pop in CR.simulatePopulation(CRsalinities, CRtemperatures):
     #print(pop)
 
-Populations = np.stack((VBpopulation, CRpopulation, CBpopulation))
+populationplot = plt.plot(time, VBpopulation) + plt.plot(time, CRpopulation) + plt.plot(time, CBpopulation)
+plt.xlabel("time points") ; plt.ylabel("population count") ; plt.title("Population of oysters over time")
 
-plt.plot(times, Populations, color="m")
+plt.show(populationplot)
+
 
 
