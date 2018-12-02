00d6ee3e3d31ac79a95176929b2f8b207e36cb3c
adoanadi <aanadi1@lsu.edu>
2018-12-02 11:02:09 -0600

    final comments

--- oysterA.py
+++ oysterA.py
@@ -4,18 +4,8 @@
 
 
 #import matplotlib as plot
+#import numpy as np
 
-#files for sites are made...now to read them
-#TEST FROM GOOGLE
-col_num = 1 #column number 1 = temperature values
-col_data = [] #placing the data numbers from column 1 (temperature) into a list
-delimiter = "|" #making the "pipe" character split a string
-with open("CalcRiv.txt") as f: #opening the file CalcRiv.txt
-    lines = f.readlines() #reading all the lines in the CalcRiv.txt file
-    for line in lines: #in going line by line in the CalcRiv.txt file
-        line = line.split(delimiter)[col_num] #in each line, column number 1 (temperature values) is split by the "pipe" character which is serving as the delimiter
-        print(line) #each resulting line will be printed to the screen
-        col_data.append(line) #appending the newly delimited lines to a file
 
 #defining the oyster population with attention to tolerances and sizes
 class oysterpopulation:
@@ -24,45 +14,72 @@ class oysterpopulation:
     def __init__(self, site, startSize, saltol, temptol):
         self.saltol = saltol
         self.temptol = temptol
-        self.startSize = startSize
+        self.size = startSize
         self.site = site
 
-    def currentSize(self):
-        return self.generations[self.generations.count - 1]
+    #pulling salinity data from USGS site files
+    #make a list of salinities from a file
+    def pullsalinity(self, fileName):
+        col_num = 2 #in the files containing salinity data, salinity values are in column 2
+        salinities = [] #salinity values will be placed into a list
+        delimiter = "|" #making the "pipe" character split a string
+        with open(fileName) as file: #opening the fileName corresponding to the buoy site
+            lines = file.readlines() #reading all the lines in the given buoy site file
+            for line in lines: #in going line by line in the given file
+                line = line.split(delimiter)[col_num] #in each line, column number 1 (temperature values) is split by the "pipe" character which is serving as the delimiter
+                salinities.append(float(line)) #appending the newly delimited lines to a file as integers/list of numbers
+        return salinities
+
+    #pulling temperature data from USGS site files
+    #make a list of temperatures from a file
+    def pulltemperature(self, fileName):
+        col_num = 1 #in the files containing temperature data, temperature values are in column 1
+        temperatures = []
+        delimiter = "|"
+        with open(fileName) as file:
+            lines = file.readlines()
+            for line in lines:
+                line = line.split(delimiter)[col_num]
+                temperatures.append(float(line))
+        return temperatures
+
 
     #updating population size as conditions change over time
-    def updatePopulation(self, time, salinity, temperature):
-        newSize = self.currentSize()[0]
-        if (salinity < self.saltol):
-            newSize = newSize * .7
-        if (salinity < 1):
-            newSize = newSize * .1
-        if (temperature > self.temptol):
-            newSize = newSize * .6
+    #need to make a list of population changes...how to do this?
+    #maybe make this two methods? 
+    #also need to include else conditions, otherwise it wont do anything
+    def updatePopulation(self, salinities, temperatures):
+        newSize = self.size
+        for sal in salinities:
+            if (sal < self.saltol):
+                newSize = newSize * .7
+            if (sal < 1):
+                newSize = newSize * .1
+        for temp in temperatures:
+            if (temp > self.temptol):
+                newSize = newSize * .6
+            if (temp == 20):
+                newSize = newSize * 1.1
+        self.size = newSize
 
-        if (salinity == 20):
-            newSize = newSize * 1.1
-#going to make updatePopulation into a list 
-#make list into an array
-#or make updatePopulation(s) from 3 different sites into one array using numpy.stack function
-#numpy.stack( LIST )
-#numpy.append ( LIST ) will be used to add numbers to the end of the array
+#next step is to make our list of lists (salinites, temperatures, populations)
 
-
+#delete size
+#self.size will be defined as a list
 
 VB = oysterpopulation("Vermillion Bay", 50, 2, 25)
+VBsalinities = VB.pullsalinity("experimentalvb.txt")
+VBtemperatures = VB.pulltemperature("experimentalvb.txt")
+VB.updatePopulation(VBsalinities, VBtemperatures)
+
 CR = oysterpopulation("Calcasieu River", 50, 5, 25)
+CRsalinities = CR.pullsalinity("CalcRiv.txt")
+CRtemperatures = CR.pulltemperature("CalcRiv.txt")
+CR.updatePopulation(CRsalinities, CRtemperatures)
+
 CB = oysterpopulation("Caillou Bay", 50, 15, 25)
+CBsalinities = CB.pullsalinity("Caillou.txt")
+CBtemperatures = CB.pulltemperature("Caillou.txt")
+CB.updatePopulation(CBsalinities, CBtemperatures)
 
-
-data = [
-    [23,34],
-    [23,34],
-    [23,34],
-    [23,34],
-    [23,34],
-    [23,34],
-]
-
-for timePeriod in data:
-    lowsal.updatePopulation("time", timePeriod[0], timePeriod[1])
+#pyplot ignore nan
--- oysterpopd.py
+++ oysterpopd.py
@@ -1,63 +1,136 @@
 #!/usr/bin/env python
-#this program simulates oyster populations in locations affected by salinity and temperature
-#the goal is to visually represent the effects of freshwater events during the summer on oyster populations
+# this program simulates oyster populations in locations affected by salinity and temperature
+# the goal is to visually represent the effects of freshwater events during the summer on oyster populations
 
+#importing/loading matplotlib.pyplot and numpy
+import matplotlib.pyplot as plt
+import numpy as np
 
-#import matplotlib as plot
-
-#files for sites are made...now to read them
-
-
-
-
-#defining the oyster population with attention to tolerances and sizes
-class oysterpopulation:
+# defining the oyster population with attention to tolerances and sizes
+class oysterpopulation: #creating oysterpopulation class
     """This is a class to define oyster populations"""
 
+    #oysterpopulation constructor, defining class parameters
     def __init__(self, site, startSize, saltol, temptol):
         self.saltol = saltol
         self.temptol = temptol
         self.startSize = startSize
         self.site = site
 
-    def salinity(self, file = "", sallistname = "")
-        col_num = 2
-        sallistname = []
-        delimiter = "|"
-        with open("file") as f:
-            lines = f.readlines()
-            for line in lines:
-                line = line.split(delimiter)[col_num]
-                sallistname.append(line)
+    #defining function to pull data from oyster buoy site files
+    def pulldata(self, fileName, columnIndex):
+        data = [] #data values will be placed in a list
+        delimiter = "|" #"pipe" character will be used to split string of values within file
+        with open(fileName) as file: #opens the file of choice
+            for line in file.readlines(): #lines will be read within the file
+                line = line.split(delimiter)[columnIndex] #"pipe" delimiter will be placed at the column number indicated
+                data.append(float(line)) #the data values will be appended to the data variable as floating/decimal numbers
+        return data #returning data values 
 
-    def temperature(self, file = "", templistname = "")
-        col_num = 1
-        templistname = []
-        delimiter = "|"
-        with open("file") as f:
-            lines = f.readlines()
-            for line in lines:
-                line = line.split(delimiter)[col_num]
-                templistname.append(line)
+    # pulling salinity data from USGS site files using pulldata function
+    # make a list of salinities from a file
+    def pullsalinity(self, fileName):
+        return self.pulldata(fileName, 2)
+
+    # pulling temperature data from USGS site files using pulldata function
+    # make a list of temperatures from a file
+    def pulltemperature(self, fileName):
+        return self.pulldata(fileName, 1)
+
+    #function to simulate population response to different salinity and temperature conditions
+    def simulatePopulation(self, salinities, temperatures):
+        populationSizes = [] #list of population sizes
+        populationSizes.append(self.startSize) #initial population size appended to list of population sizes
+        environments = zip(salinities, temperatures) #environment consists of compound effects of salinity and temperature
+        salinityTimer = 240 #salinity conditions will persist for 5 days ((5 days)(48 data points per day retrieved from USGS))
+        temperatureTimer = 240 #temperature conditions will persist for 5 days ((5 days)(48 data points per day retrieved from USGS))
+        for environment in environments:
+            salinity = environment[0] #salinity values will make up the 1st column in environments list
+            temperature = environment[1] #temperature values will make up the 2nd column in environments list
+            newSize = populationSizes[-1] #new population size corresponding to salinity and temperature values will make up 3rd column in environments list
+            if (salinity < self.saltol): #if salinity conditions are less than oyster salinity tolerance
+                salinityTimer = salinityTimer - 48 #salinity timer is reduced 48 data points (1 day); essentially salinity conditions will persist until the entire 5 days has passed
+                if (salinityTimer <= 0): #after the 5 days under the less than ideal salinity conditions
+                    newSize = newSize * .90 #90% of the oyster population will remain
+            else:
+                salinityTimer = salinityTimer + 48 #as time progresses (48 = time points per day)
+            if (salinity < 1): #if salinity is less than 1
+                salinityTimer = salinityTimer - 48 #as salinity conditions persist for 5 days total
+                if (salinityTimer <= 0): #if salinity conditions remain at the end of the 5 days
+                    newSize = newSize * .60 #new oyster population size will decrease by 40%
+            if (temperature > self.temptol): #if the temperature is greater than oyster temperature tolerance 
+                temperatureTimer = temperatureTimer - 48 #(temperature conditions persist for total of 5 days)
+                if (temperatureTimer <= 0): #after the 5 days under the temperature conditions
+                    newSize = newSize * .99 #99% of the oyster population will remain
+            else:
+                temperatureTimer = temperatureTimer + 48 #as time progresses (48 = time points per day)
+            if (temperature == 20): #if temperature is 20 degrees Celsius
+                temperatureTimer = temperatureTimer - 48 #temperature conditions will continue each day for 5 days
+                if (temperatureTimer <= 0): #if temperature conditions reach the end of the 5 days
+                    newSize = newSize * 1.5 #new population size will increase, oysters will spawn
+            else:
+                temperatureTimer = temperatureTimer + 48 
+            populationSizes.append(newSize) #every 5 days, new oyster population size determined by environment conditions
+                                            #will be appended to list of oyster population sizes
+
+            #reseting all the condition timers
+            #after 5 days is up, under the temperature conditions and under the salinity conditions, both timers will reset
+            if temperatureTimer > 240:  
+                temperatureTimer = 240 
+            if salinityTimer > 240: 
+                salinityTimer = 240 
+            if temperatureTimer < 0: 
+                temperatureTimer = 0
+            if salinityTimer < 0:
+                salinityTimer = 0
+        return populationSizes[1:] #returns the values of the oyster population sizes
 
 
-    #updating population size as conditions change over time
-    #we're going to need a for loop 
-    def updatePopulation(self, sallistname, templistname):
-        newSize = self.newSize()[0]
-        if (salinity < self.saltol):
-            newSize = newSize * .7
-        if (salinity < 1):
-            newSize = newSize * .1
-        if (temperature > self.temptol):
-            newSize = newSize * .6
+time = np.arange(2177) #time points will consist of range of 2177 values
 
-        if (salinity == 20):
-            newSize = newSize * 1.1
+#defining parameters for each oyster population location
 
-VB = oysterpopulation("Vermillion Bay", 50, 2, 25)
-CR = oysterpopulation("Calcasieu River", 50, 5, 25)
-CB = oysterpopulation("Caillou Bay", 50, 15, 25)
+#site = Vermillion Bay, starting population size = 10000, salinity tolerance = 5, temperature tolerance = 29
+#blue
+VB = oysterpopulation("Vermillion Bay", 10000, 5, 29) 
+VBsalinities = VB.pullsalinity("VBAugSep.txt") #salinity data modified/pulled from VBAugSept.txt file
+VBtemperatures = VB.pulltemperature("VBAugSep.txt") #temperature data modified/pulled from VBAugSept.txt file
+VBpopulation = VB.simulatePopulation(VBsalinities, VBtemperatures) #oyster population simulation for location
 
+#site = Calcasieu River, starting population size = 10000, salinity tolerance = 7, temperature tolerance = 29
+#orange
+CR = oysterpopulation("Calcasieu River", 10000, 7, 29)
+CRsalinities = CR.pullsalinity("CRAugSep.txt") #salinity data modified/pulled from CRAugSep.txt file
+CRtemperatures = CR.pulltemperature("CRAugSep.txt") #temperature data modified/pulled from CRAugSep.txt file
+CRpopulation = CR.simulatePopulation(CRsalinities, CRtemperatures) #oyster population simulation for location
 
+#site = Caillou Bay, starting population size = 10000, salinity tolerance = 14, temperature tolerance = 29
+#green
+CB = oysterpopulation("Caillou Bay", 10000, 14, 29)
+CBsalinities = CB.pullsalinity("CBAugSep.txt") #salinity data modified/pulled from CBAugSep.txt file
+CBtemperatures = CB.pulltemperature("CBAugSep.txt") #temperature data modified/pulled from CBAugSep.txt file
+CBpopulation = CB.simulatePopulation(CBsalinities, CBtemperatures) #oyster population simulation for location
 
+#plotting oyster population data with time on x-axis and population data for each site on y-axis
+populationplot = plt.plot(time, VBpopulation) + plt.plot(time, CRpopulation) + plt.plot(time, CBpopulation)
+plt.xlabel("Time point") ; plt.ylabel("Population Count") ; plt.title("Population of oysters over time") #setting axes titles and figure title
+
+plt.show(populationplot) #display resulting figure
+
+plt.savefig("PopulationData.png") #save resulting figure in a file PopulationData.png
+
+#plotting salinity changes over time for each location with time on x-axis and site salinities on y-axis
+salinityplot = plt.plot(time, VBsalinities) + plt.plot(time, CRsalinities) + plt.plot(time, CBsalinities)
+plt.xlabel("Time point") ; plt.ylabel("Salinity (ppt)") ; plt.title("Salinity over time") #setting axes titles and figure title
+
+plt.show(salinityplot) #display resulting salinity figure
+
+plt.savefig("salinityplot.png") #save resulting figure in a file salinityplot.png
+
+#plotting temperature changes over time for each location with time on x-axis and site temperatures on y-axis
+temperatureplot = plt.plot(time, VBtemperatures) + plt.plot(time, CRtemperatures) + plt.plot(time, CBtemperatures)
+plt.xlabel("Time point") ; plt.ylabel("Temperature (Celcius)") ; plt.title("Temperature over time") #setting axes titles and figure title
+
+plt.show(temperatureplot) #display resulting temperature figure
+
+plt.savefig("temperatureplot.png") #save resulting figure in a file temperatureplot.png
